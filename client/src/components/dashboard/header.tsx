107108b0b64018ca4b8858f6f7e4e38e98a6dea7
Your Name <you@example.com>
2025-08-02 22:41:57 +0530

    refactor: simplify Avatar component and remove Skeleton component for cleaner structure

--- client/src/components/dashboard/header.tsx
+++ client/src/components/dashboard/header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/avatar";
+import { Avatar } from "@/components/avatar";
 import { Button } from "@/components/button";
 import {
   DropdownMenu,
@@ -52,10 +52,7 @@ export default function Header() {
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-                <Avatar className="h-8 w-8">
-                  <AvatarImage src="" alt="User" />
-                  <AvatarFallback>U</AvatarFallback>
-                </Avatar>
+                <Avatar className="h-8 w-8" src="" alt="User" fallback="U" />
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56" align="end" forceMount>
